fe81c026f6406cfdcec39a05a1b67de91ee83bfa
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2022-08-03 23:23:13 +0900

    :sparkles:IP アドレスを追加するパターンを実装する #4

--- function.sh
+++ function.sh
@@ -19,8 +19,16 @@ TOKEN=$(aws wafv2 get-ip-set --name "${IPSETS_NAME}" --id "${ID}" --scope "${SCO
         | grep "LockToken" | tr -d \" | awk '{print $2}')
 echo "${TOKEN}"
 
-aws wafv2 update-ip-set --name "${IPSETS_NAME}" --id "${ID}" --addresses "${IP_ADDRESS}" \
+# echo -n "REGISTERED_IP_ADDRESSES:"  # debug message
+REGISTERED=$(aws wafv2 get-ip-set --name "${IPSETS_NAME}" --id "${ID}" --scope "${SCOPE}" --profile "${PROFILE}" \
+        | jq '.IPSet.Addresses' | grep -v "\[" | grep -v "\]" | tr -d ' ' | tr -d '"' | tr -d "\n")
+# echo "${REGISTERED}"  # debug message
+
+UPDATE_IP_ADDESSES=$(echo '["'"${REGISTERED},$IP_ADDRESS"'"]' | sed -e 's/,/", "/g')
+echo "UPDATE_IP_ADDESSES:${UPDATE_IP_ADDESSES}"
+aws wafv2 update-ip-set --name "${IPSETS_NAME}" --id "${ID}" --addresses "${UPDATE_IP_ADDESSES}" \
     --lock-token "${TOKEN}" --scope "${SCOPE}" --profile "${PROFILE}" \
     > /dev/null
 
+# 追加されたことを確認するため get-ip-set を実行する
 aws wafv2 get-ip-set --name "${IPSETS_NAME}" --id "${ID}" --scope "${SCOPE}" --profile "${PROFILE}"
