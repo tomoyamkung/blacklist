5362afbf00eabdd2d7e95586163213edd607fdc6
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2022-08-04 09:50:55 +0900

    :sparkles:プロファイル名を --profile オプションで受け取る処理を実装する #2

--- function.sh
+++ function.sh
@@ -2,12 +2,56 @@
 # shellcheck disable=SC3040
 set -euo pipefail
 
-PROFILE=$1
-IP_ADDRESS="${2}/32"  # TODO 保管される旨を README に追記する
+
+function usage() {
+    cat <<EOF 1>&2
+Description:
+    $(basename ${0}) is a tool for ...
+
+Usage:
+    $(basename ${0}) --profile PROFILE_NAME ip_address
+
+Options:
+    --help          print this
+    --profile       TDB
+EOF
+}
+
+
+while (( $# > 0 ))
+do
+    case $1 in
+        --help)
+            usage
+            exit 1
+            ;;
+        --profile)
+            if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+                echo "'option' requires an argument." 1>&2
+                exit 1
+            else
+                PROFILE="${2}"
+                echo "PROFILE:${PROFILE}"
+                shift
+            fi
+            ;;
+        -*)
+            usage
+            exit 1
+            ;;
+        *)
+            IP_ADDRESS="${1}/32"  # TODO 保管される旨を README に追記する
+            echo "IP_ADDRESS:${IP_ADDRESS}"
+            ;;
+    esac
+    shift
+done
+
+
+# 定数定義
 SCOPE=REGIONAL  # CloudFront は対象外なので REGIONAL で固定
 IPSETS_NAME=BLACK_LIST  # 対象とする IP Sets の名前は BLACK_LIST で固定
 
-echo "IP_ADDRESS:${IP_ADDRESS}"
 
 echo -n "ID:"
 ID=$(aws wafv2 list-ip-sets --output table --scope "${SCOPE}" --profile "${PROFILE}" \
